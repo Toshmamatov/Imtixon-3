0b2fb81f996d5f3671f4455c8464d3db8a98831e
Toshmamatov <toshmamatov03@gmail.com>
2022-01-11 13:26:42 -0700

    save

--- js/index.js
+++ js/index.js
@@ -3,22 +3,28 @@ var elInput = document.querySelector(".form__input");
 var elSelectType = document.querySelector(".select__type");
 var elList = document.querySelector(".list");
 
-var genres = [];
+elForm.addEventListener("submit", function (even) {
+  even.preventDefault();
+  console.log(elInput.value);
+});
 
-for (var i = 0; i < pokemons.length; i++) {
-  var item = document.createElement("li");
-  var img = document.createElement("img");
-  var title = document.createElement("h3");
-  var type = document.createElement("h4");
-  var weight = document.createElement("span");
-  var height = document.createElement("span");
-  var icon = document.createElement("img");
+function elementCreator(tagName) {
+  return document.createElement(tagName);
+}
 
-  img.src = pokemons[i].img;
-  title.textContent = pokemons[i].name;
-  type.textContent = pokemons[i].type;
-  weight.textContent = pokemons[i].weight;
-  height.textContent = pokemons[i].height;
+function filmCreator(pokemon) {
+  var item = elementCreator("li");
+  var img = elementCreator("img");
+  var title = elementCreator("h3");
+  var type = elementCreator("h4");
+  var weight = elementCreator("span");
+  var height = elementCreator("span");
+
+  img.src = pokemon.img;
+  title.textContent = pokemon.name;
+  type.textContent = pokemon.type;
+  weight.textContent = pokemon.weight;
+  height.textContent = pokemon.height;
 
   item.appendChild(img);
   item.appendChild(title);
@@ -27,9 +33,17 @@ for (var i = 0; i < pokemons.length; i++) {
   item.appendChild(height);
 
   elList.appendChild(item);
+}
 
-  //pokemon type dropdown
+var genres = [];
 
+for (var i = 0; i < pokemons.length; i++) {
+  filmCreator(pokemons[i]);
+}
+
+//pokemon type dropdown
+
+for (var i = 0; i < pokemons.length; i++) {
   pokemons[i].type.forEach(function (item) {
     if (!genres.includes(item)) {
       genres.push(item);
@@ -41,9 +55,4 @@ genres.forEach((item) => {
   var selectChild = document.createElement("option");
   selectChild.textContent = item;
   elSelectType.appendChild(selectChild);
-});
-
-elForm.addEventListener("submit", function (even) {
-  even.preventDefault();
-  console.log(elInput.value);
 });
